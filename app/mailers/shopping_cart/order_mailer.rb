b728227705376787b7272bba7e7eafe9dcfcc1d9
ighnatenko <ighnatenko@meta.ua>
2018-07-09 16:48:37 +0300

    refactoring

---
+++ app/mailers/shopping_cart/order_mailer.rb
@@ -0,0 +1,8 @@
+module ShoppingCart
+  class OrderMailer < ApplicationMailer
+    def order_confirmation(user, order)
+      @url = order_confirm_url(order, order.confirmation_token)
+      mail(to: user.email, subject: "Please confirm your order to continue")
+    end
+  end
+end
